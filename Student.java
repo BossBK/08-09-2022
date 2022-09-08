083097142bc0e7ace266aece99e6450caa80c3cf
BossBK <110155558+BossBK@users.noreply.github.com>
2022-09-08 12:41:51 +0100

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,84 @@
+package com.equals.demo;
+
+import java.util.Objects;
+
+public class Student {
+	
+	public String Name;
+	public int Rollno;
+	public String Standeard;
+	public String Section;
+	public String Performance;
+	
+	
+	public Student() {
+		
+	}
+	
+	public Student(String Name, int Rollno, String Standeard, String Section, String Performance ) {
+		
+		this.Name = Name;
+		this.Rollno = Rollno;
+		this.Standeard = Standeard;
+		this.Section = Section;
+		this.Performance = Performance;
+	}
+
+	public String getName() {
+		return Name;
+	}
+
+	public void setName(String name) {
+		Name = name;
+	}
+
+	public int getRollno() {
+		return Rollno;
+	}
+
+	public void setRollno(int rollno) {
+		Rollno = rollno;
+	}
+
+	public String getStandeard() {
+		return Standeard;
+	}
+
+	public void setStandeard(String standeard) {
+		Standeard = standeard;
+	}
+
+	public String getSection() {
+		return Section;
+	}
+
+	public void setSection(String section) {
+		Section = section;
+	}
+
+	public String getPerformance() {
+		return Performance;
+	}
+
+	public void setPerformance(String performance) {
+		Performance = performance;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Student other = (Student) obj;
+		
+		return Objects.equals(Name, other.Name) && Objects.equals(Performance, other.Performance)
+				&& Rollno == other.Rollno && Objects.equals(Section, other.Section)
+				&& Objects.equals(Standeard, other.Standeard);
+	}
+	
+	
+	
+}
---
+++ StudentTest.java
@@ -0,0 +1,18 @@
+package com.equals.demo;
+
+public class StudentTest {
+
+	public static void main(String[] args) {
+		
+		Student stu1 = new Student("lokesh", 04, "10th", "B", "Good");
+		Student stu2 = new Student("Rakesh", 02, "11th", "C", "Bad");
+		Student stu3 = new Student("Abishek", 05, "11th", "A", "Average");
+		Student stu4 = new Student("Nithish", 06, "10th", "B", "Good");
+		
+		System.out.println(stu1.equals(stu4));
+		System.out.println(stu3.equals(stu2));
+		System.out.println(stu2.equals(stu1));
+		System.out.println(stu1.equals(stu3));
+	}
+
+}
